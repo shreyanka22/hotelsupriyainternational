831c351ad6b1c6f71b1317fd2b28a9cb7356e119
Shreyanka Naik <123201052+shreyanka22@users.noreply.github.com>
2026-03-07 22:04:16 +0530

    fix render production config

--- backend/server.js
+++ backend/server.js
@@ -31,11 +31,17 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  // cookie: {
+  //   secure: false,       // set true in production with HTTPS
+  //   httpOnly: true,
+  //   maxAge: 1000 * 60 * 60 * 24, // 1 day
+  // },
   cookie: {
-    secure: false,       // set true in production with HTTPS
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24, // 1 day
-  },
+  secure: true,
+  sameSite: "none",
+  httpOnly: true,
+  maxAge: 1000 * 60 * 60 * 24,
+}
 }))
 app.use(passport.initialize())
 app.use(passport.session())
@@ -48,6 +54,6 @@ app.use("/api/reviews",   reviewRoutes)
 /* ── Health check ── */
 app.get("/api/health", (req, res) => res.json({ status: "ok", message: "Hotel Supriya API running" }))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT 
 app.listen(PORT, () => console.log(`🏨 Server running on http://localhost:${PORT}`))
 
